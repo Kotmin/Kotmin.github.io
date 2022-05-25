d91309d04de1d69c9f84d2ea9eda4fe94486015b
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-05-25 21:24:09 +0200

    Looks like working and responsive navbar
    I have to check walidators and think about delete borders on menu at smaller screens

--- js_scripts/functions.js
+++ js_scripts/functions.js
@@ -21,12 +21,29 @@ function switch_theme(){
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
     
-    switch_theme();
+    
     
 
     burger.addEventListener('click',() => {nav.classList.toggle('nav-active');
+    
+        //Animation of links
+    navLinks.forEach((link,index) => {
+        if(link.style.animation){
+            link.style.animation ='';
+        }
+        else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
+        }
+    });
+//      //Burger animation
+    burger.classList.toggle('toggle');
+        
 });
+
+
+
 };
 
 //const app = ()=>{
