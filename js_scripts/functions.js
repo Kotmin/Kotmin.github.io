a1a21a128b7e01062fb9e578b609a2ff886dc4ee
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-05-27 00:05:58 +0200

    WORKING HUD

--- js_scripts/functions.js
+++ js_scripts/functions.js
@@ -129,8 +129,31 @@ function showGamePage(){
     var text="";
     
     text+= `
-<div class="container>
+<div class="container">
         <div id="game" class="guizdiv justify-center flex-column">
+            <div id="hud">
+                <div id="hud-item">
+                    <p class="hud-prefix">
+                    Question
+                    </p>
+                    <h1 class="hud-main-text" id="questionCounter">
+                    
+                    </h1>
+                </div>
+
+    
+   
+                <div id="hud-item">
+                    <p class="hud-prefix">
+                    Score
+                    </p>
+                    <h2 class="hud-main-text" id="score">
+                    0
+                    </h2>
+                </div>
+            
+        </div>
+        
             <h2 id="question"> What is the answer to this questions?</h2>
     
             <div class="choice-container">
@@ -182,6 +205,8 @@ function showGamePage(){
 runQuizGame = () => {
     const question = document.getElementById('question');
     const choices = Array.from(document.getElementsByClassName('choice-text'));
+    const questionCounterText= document.getElementById('questionCounter');
+    const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -237,6 +262,9 @@ getNewQuestion = () => {
 //        tu jest zakonczenie quizu
     }
     questionCounter++;
+//    questionCounterText.innerText = questionCounter+ "/" + MAX_QUESTIONS;
+    questionCounterText.innerText =` ${questionCounter}/${MAX_QUESTIONS} `;
+    
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -260,6 +288,11 @@ choices.forEach((choice) => {
 //        console.log(selectedAnswer == currentQuestion.answer);
           const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
           
+          if(classToApply==='correct'){
+              incrementScore(CORRECT_BONUS);
+          }
+          
+          
 //          sypnie bledem czy nie sypnie?
           
           
@@ -274,19 +307,17 @@ choices.forEach((choice) => {
           
           
          
-          
-          
-//        const classToApply = 'incorrect';
-//        if (selectedAnswer == currentQuestion.answer) {
-//            classToApply = 'correct';
-//        }
-//        
-//        
+   
         
         
         
     });
 });
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+};
 
 startGame();
     
@@ -295,87 +326,4 @@ startGame();
 
 
 
-//
-////const question = document.getElementById('question');
-////const choices = Array.from(document.getElementsByClassName('choice-text'));
-//
-//let currentQuestion = {};
-//let acceptingAnswers = false;
-//let score = 0;
-//let questionCounter = 0;
-//let availableQuesions = [];
-//
-//let questions = [
-//    {
-//        question: 'Inside which HTML element do we put the JavaScript??',
-//        choice1: '<script>',
-//        choice2: '<javascript>',
-//        choice3: '<js>',
-//        choice4: '<scripting>',
-//        answer: 1
-//    },
-//    {
-//        question:
-//            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-//        choice1: "<script href='xxx.js'>",
-//        choice2: "<script name='xxx.js'>",
-//        choice3: "<script src='xxx.js'>",
-//        choice4: "<script file='xxx.js'>",
-//        answer: 3
-//    },
-//    {
-//        question: " How do you write 'Hello World' in an alert box?",
-//        choice1: "msgBox('Hello World');",
-//        choice2: "alertBox('Hello World');",
-//        choice3: "msg('Hello World');",
-//        choice4: "alert('Hello World');",
-//        answer: 4
-//    }
-//];
-//
-//
-////CONSTANTS
-//const CORRECT_BONUS = 10;
-//const MAX_QUESTIONS = 3;
-//
-//startGame = () => {
-//    questionCounter = 0;
-//    score = 0;
-//    availableQuesions = [...questions];
-////    operator rozdzielenia
-//    getNewQuestion();
-//};
-//
-//getNewQuestion = () => {
-//    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-//        //go to the end page
-//        return window.location.assign('/end.html');
-//    }
-//    questionCounter++;
-//    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-//    currentQuestion = availableQuesions[questionIndex];
-//    question.innerText = currentQuestion.question;
-//
-//    choices.forEach((choice) => {
-//        const number = choice.dataset['number'];
-//        choice.innerText = currentQuestion['choice' + number];
-//    });
-//
-//    availableQuesions.splice(questionIndex, 1);
-//    acceptingAnswers = true;
-//};
-//
-//choices.forEach((choice) => {
-//    choice.addEventListener('click', (e) => {
-//        if (!acceptingAnswers) return;
-//
-//        acceptingAnswers = false;
-//        const selectedChoice = e.target;
-//        const selectedAnswer = selectedChoice.dataset['number'];
-//        getNewQuestion();
-//    });
-//});
-//
-//startGame();
-//
-//
+
